107b40b38216b258a3c5ad8ae3dd2e2e55a32393
Vinicus Soares <vinicius_pereirasoares@hotmail.com>
2023-02-04 03:59:54 -0300

    mudança no visual

--- js/get.js
+++ js/get.js
@@ -8,33 +8,28 @@ if(ul){
 
          dados.forEach(item => { //loop de cada elemento
             htmlLi.push(`
-                <div class="row">
-                <div class="col-md-4">
-                    <div class="widget lazur-bg p-x1">
-                        <h2>${item.nome}</h2>
-                        <ul class="list-unstyled m-t-md">
-                            <li>
-                                <span class="fa fa-envelope m-r-xs"></span>
-                                <label>Email:</label>
-                                ${item.email} 
-                            </li>
-                            <li>
-                                <span class="fa fa-phone m-r-xs"></span>
-                                <label>Fone:</label>
-                                 ${item.fone} 
-                            </li>
-                            <li>
-                            <span class="fa fa-phone m-r-xs"></span>
-                            <label>Descrição:</label>
-                             ${item.descricao} 
-                            </li>
-                        </ul>
-                        <div class="pull-right btn-group-sm d-flex justify-content-end">
-                            <button type="button" class="btn btn-primary" onclick="deleteItem(${item.id})">Excluir</button>
-                        </div>
-                    </div>   
-                </div>
-            </div>`);
+            <h5 class="h5 g-color-gray-dark-v1 mb-0">${item.nome}</h5>
+            <ul class="list-unstyled m-t-md">
+                <li>
+                    <span class="fa fa-envelope m-r-xs"></span>
+                    <label>Email:</label>
+                    ${item.email} 
+                </li>
+                <li>
+                    <span class="fa fa-phone m-r-xs"></span>
+                    <label>Fone:</label>
+                     ${item.fone} 
+                </li>
+            </ul>
+            <h6>Messagem</h6>
+          <p>${item.descricao}</p>
+          <div class="container d-flex justify-content-end">
+          <button type="button" class="btn btn-sm btn-outline-danger" onClick="deleteItem(${item.id})"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+          <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+        </svg></button>
+          </div>  
+          <hr>`);
          });
          ul.innerHTML = htmlLi;
         })
